8efceffcedc0309f770e16c75f2c6744fccc187c
joseph sagayaraj <bjsahay@gmail.com>
2018-01-22 10:42:42 +0100

    add nav component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { CourseComponent } from './course/course.component';
 import { NewComponent } from './new/new.component';
+import { NavComponent } from './nav/nav.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     CourseComponent,
-    NewComponent
+    NewComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule
