3c0422bba0630ab5f0cb6b28e75cf726dfed2a15
joseph sagayaraj <bjsahay@gmail.com>
2018-01-22 19:28:31 +0100

    create new components

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-course',
-  templateUrl: './course.component.html',
-  styleUrls: ['./course.component.css']
+  selector: 'courses',
+  template: `
+            <h2>{{title}}</h2>
+            <ul>
+              <li *ngFor ="let course of courses">{{course}}</li>
+            </ul>
+            <img  src="{{imageUrl}}"/>
+            <br/>
+            <button [style.backgroundColor]="isActive ? 'lightblue' : 'white'">Save</button>
+            `
 })
-export class CourseComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class CourseComponent {
+  title = "List of courses";
+  courses = ["english", "science","french"];
+  imageUrl = "http://lorempixel.com/400/200";
+  isActive = true;
+  
 }
